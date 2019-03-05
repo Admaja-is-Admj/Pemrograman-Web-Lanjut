897f80d1baf7ce94c787e88ee68b4cf1c9783bc1
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2019-03-05 11:27:18 +0700

    upload file angular

---
+++ Praktikum2/ts-hello/main.js
@@ -0,0 +1,14 @@
+// function log (message){
+//     console.log(message);
+// }
+// let message = 'Hello World';
+// log(message);
+// function doSomething(){
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
+//     console.log('Finally: '+ i);
+// }
+// doSomething();
+var count = 5;
+count = 'a';
---
+++ Praktikum2/ts-hello/main.ts
@@ -0,0 +1,16 @@
+// function log (message){
+//     console.log(message);
+// }
+
+// let message = 'Hello World';
+
+// log(message);
+// function doSomething(){
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
+//     console.log('Finally: '+ i);
+// }
+// doSomething();
+let count = 5;
+count = 'a';

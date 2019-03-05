02bbe15db4ff9fe0a8372e9aacdfc571cddba03b
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2019-03-05 14:08:18 +0700

    upload file Praktikum4

---
+++ Praktikum4/src/app/courses/courses.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from './courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+  title = 'Belajar Angular';
+  binding = 'property-binding';
+  imageUrl = 'http://lorempixel.com/400/200';
+  colSpan = 2;
+  isActive = false;
+  nama = 'Roberto';
+  onSave($event){
+    $event.stopPropagation();
+    console.log('button sudah diklik')
+  }
+  onDivClick($event){
+    console.log("ini method div",$event)
+  }
+  onKeyUp(){
+    console.log("enter was pressed");
+  }
+  // title = 'Angular';
+  // point = '3 Author';
+  Courses;
+  constructor(private service:CoursesService) {
+    // this.Courses = service.getCourses(); 
+    // this.Courses = service.getCourses();
+  }
+  ngOnInit() {
+  }
+
+}

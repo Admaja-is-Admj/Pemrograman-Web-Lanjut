c791da73a9cc7fc117ed212403b7c6fd34c18554
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2019-04-24 14:00:37 +0700

    9. melakukan modifikasi pada post.component.ts

--- HTTPService/src/app/app.module.ts
+++ HTTPService/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { PostsComponent } from './posts/posts.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- HTTPService/src/app/posts/posts.component.ts
+++ HTTPService/src/app/posts/posts.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from  '@angular/http';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class PostsComponent {
+  
+  constructor(http:Http) {
+     
   }
-
 }
